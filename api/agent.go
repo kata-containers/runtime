f491d42de1e65dae0d2bc2ad39c76c5222a1105b
Lai Jiangshan <jiangshanlai@gmail.com>
2018-03-07 11:54:28 +0800

    make agent's method private
    Signed-off-by: Lai Jiangshan <jiangshanlai@gmail.com>

--- api/agent.go
+++ api/agent.go
@@ -1,12 +1,5 @@
 package api
 
-import (
-	"encoding/json"
-	"syscall"
-
-	ocispecs "github.com/opencontainers/runtime-spec/specs-go"
-)
-
 type AgentConfig struct {
 	// ConnProto: the method to connect to agent(or proxy)
 	// "unix": the paths are unix socket
@@ -26,30 +19,31 @@ type AgentConfig struct {
 
 type Agent interface {
 	Config() AgentConfig
-	Dump() json.RawMessage
-	Release() error
-	Destroy()
 
-	PauseSync() error
-	Unpause() error
-
-	CreateContainer(container string, user *UserGroupInfo, storages []*Storage, c *ocispecs.Spec) error
-	StartContainer(container string) error
-	ExecProcess(container, process string, user *UserGroupInfo, p *ocispecs.Process) error
-	SignalProcess(container, process string, signal syscall.Signal) error
-	WaitProcess(container, process string) int
-
-	StdioPipe(container, process string) (stdinPipe io.WriteCloser, stdout io.Reader, stderr io.Reader, err error)
-	TtyWinResize(container, process string, row, col uint16) error
-
-	StartSandbox(sb *SandboxNetworkConfig, storages []*Storage) error
-	DestroySandbox() error
-	UpdateRoute(r []Route) error
-	UpdateInterface(ifs []Interface) error
-	//OnlineCpuMem() error
+	// methods are private in the lib
 }
 
-// proxies, shim should be implemented via AgentBuilder (and Agent.CreateContainer() Agent.ExecProcess())
 type AgentBuilder interface {
 	Build(AgentConfig) Agent
 }
+
+// return the default AgentBuilder provided by the lib
+func DirectConnect() AgentBuilder {
+}
+
+// Example code for how to use AgentBuilder with proxy
+// The way how to luanch proxy is user (an oci-runtime-cli implementation) specific.
+// So this code can be put in runtime code.
+//
+// Proxy is an implementation of kata-agent-api, it fits for AgentBuilder
+//
+// struct proxyAgent {
+//    proxyBinary string
+//    // other fields
+// }
+// func (p proxyAgent) Build(configToVM AgentConfig) Agent {
+//    var configToProxy AgentConfig
+//    configToProxy, err := p.launchProxy(configToVM)
+//    if err != nil { return nil }
+//    return DirectConnect().Build(configToProxy)
+// }
