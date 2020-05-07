74f7fda2496ca74695d4995b3c3f40586646381d
Julio Montes <julio.montes@intel.com>
2020-05-07 20:19:49 +0000

    virtcontainers: Do not lazy-attach devices
    The "lazy attach" mechanism [1] was added to hotplugs LBS (Large BAR space)
    devices after re-scanning the PCI bus, fixing LBS hotplug in kata containers.
    Since PCI rescan is removed in kata-containers/agent#782, lazy attach is not
    longer needed.
    
    fixes #2664
    
    [1] https://github.com/kata-containers/runtime/pull/2461
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- virtcontainers/sandbox_test.go
+++ virtcontainers/sandbox_test.go
@@ -534,7 +534,7 @@ func TestSandboxAttachDevicesVFIO(t *testing.T) {
 
 	containers[c.id].sandbox = &sandbox
 
-	err = containers[c.id].attachDevices(c.devices)
+	err = containers[c.id].attachDevices()
 	assert.Nil(t, err, "Error while attaching devices %s", err)
 
 	err = containers[c.id].detachDevices()
@@ -629,7 +629,7 @@ func TestSandboxAttachDevicesVhostUserBlk(t *testing.T) {
 
 	containers[c.id].sandbox = &sandbox
 
-	err = containers[c.id].attachDevices(c.devices)
+	err = containers[c.id].attachDevices()
 	assert.Nil(t, err, "Error while attaching vhost-user-blk devices %s", err)
 
 	err = containers[c.id].detachDevices()
