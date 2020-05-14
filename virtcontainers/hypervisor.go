802bc9999380532b6cddf9c0f16555ee78060571
Christophe de Dinechin <dinechin@redhat.com>
2020-05-14 20:18:18 +0200

    config: Protect virtio_fs_daemon annotation
    Sending the virtio_fs_daemon annotation can be used to execute
    arbitrary code on the host. In order to prevent this, restrict the
    values of the annotation to a list provided by the configuration
    file.
    
    Fixes: #3004
    
    Signed-off-by: Christophe de Dinechin <dinechin@redhat.com>

--- virtcontainers/hypervisor.go
+++ virtcontainers/hypervisor.go
@@ -278,6 +278,9 @@ type HypervisorConfig struct {
 	// HypervisorPath is the hypervisor executable host path.
 	HypervisorPath string
 
+	// HypervisorPathList is the list of hypervisor paths names allowed in annotations
+	HypervisorPathList []string
+
 	// HypervisorCtlPath is the hypervisor ctl executable host path.
 	HypervisorCtlPath string
 
@@ -311,6 +314,9 @@ type HypervisorConfig struct {
 
 	// VirtioFSDaemon is the virtio-fs vhost-user daemon path
 	VirtioFSDaemon string
+
+	// VirtioFSDaemonList is the list of valid virtiofs names for annotations
+	VirtioFSDaemonList []string
 
 	// VirtioFSCache cache mode for fs version cache or "none"
 	VirtioFSCache string
