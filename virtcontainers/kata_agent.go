50c1dce137bb3d608daa931c01e4941ed5fdb6cc
Bo Chen <chen.bo@intel.com>
2020-06-01 17:58:58 -0700

    kata_agent: Pass "VirtPath" with "PCIAddr" of blk devices to agent
    In case the "PCIAddr" of block devices is not available (e.g.
    cloud-hypervisor), we also pass the "VirtPath" to the agent for adding
    block devices to the container.
    
    Fixes: #2720
    
    Signed-off-by: Bo Chen <chen.bo@intel.com>

--- virtcontainers/kata_agent.go
+++ virtcontainers/kata_agent.go
@@ -1179,6 +1179,7 @@ func (k *kataAgent) appendBlockDevice(dev ContainerDevice, c *Container) *grpc.D
 	case config.VirtioBlock:
 		kataDevice.Type = kataBlkDevType
 		kataDevice.Id = d.PCIAddr
+		kataDevice.VmPath = d.VirtPath
 	case config.VirtioSCSI:
 		kataDevice.Type = kataSCSIDevType
 		kataDevice.Id = d.SCSIAddr
