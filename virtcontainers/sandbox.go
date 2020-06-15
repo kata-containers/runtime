74a425837b241e1fe093e59a4c990f17a54951fe
Julio Montes <julio.montes@intel.com>
2020-06-15 10:37:28 -0500

    virtcontainer/cgroup: create cgroup manager after creating the network
    Create the cgroup manager once the network has been created, this way the
    list of device will include the network VFIO devices attached to the sandbox,
    when the physical enpoint is the network driver.
    
    fixes #2774
    
    Signed-off-by: Julio Montes <julio.montes@intel.com>

--- virtcontainers/sandbox.go
+++ virtcontainers/sandbox.go
@@ -606,10 +606,6 @@ func newSandbox(ctx context.Context, sandboxConfig SandboxConfig, factory Factor
 		}
 	}
 
-	if err := s.createCgroupManager(); err != nil {
-		return nil, err
-	}
-
 	agentConfig, err := newAgentConfig(sandboxConfig.AgentType, sandboxConfig.AgentConfig)
 	if err != nil {
 		return nil, err
@@ -737,6 +733,12 @@ func fetchSandbox(ctx context.Context, sandboxID string) (sandbox *Sandbox, err 
 	sandbox, err = createSandbox(ctx, config, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create sandbox with config %+v: %v", config, err)
+	}
+
+	if sandbox.config.SandboxCgroupOnly {
+		if err := sandbox.createCgroupManager(); err != nil {
+			return nil, err
+		}
 	}
 
 	// This sandbox already exists, we don't need to recreate the containers in the guest.
