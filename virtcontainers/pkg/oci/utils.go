6a5eb0ded542e4f10b88fc5a95b7c9d02959d84b
James O. D. Hunt <james.o.hunt@intel.com>
2020-10-29 09:51:13 +0000

    annotations: Improve asset annotation handling
    Make `asset.go` the arbiter of asset annotations by removing all asset
    annotations lists from other parts of the codebase.
    
    This makes the code simpler, easier to maintain, and more robust.
    
    Specifically, the previous behaviour was inconsistent as the following
    ways:
    
    - `createAssets()` in `sandbox.go` was not handling the following asset
      annotations:
    
        - firmware:
          - `io.katacontainers.config.hypervisor.firmware`
          - `io.katacontainers.config.hypervisor.firmware_hash`
    
        - hypervisor:
          - `io.katacontainers.config.hypervisor.path`
          - `io.katacontainers.config.hypervisor.hypervisor_hash`
    
        - hypervisor control binary:
          - `io.katacontainers.config.hypervisor.ctlpath`
          - `io.katacontainers.config.hypervisor.hypervisorctl_hash`
    
        - jailer:
          - `io.katacontainers.config.hypervisor.jailer_path`
          - `io.katacontainers.config.hypervisor.jailer_hash`
    
    - `addAssetAnnotations()` in the `oci` package was not handling the
      following asset annotations:
    
        - hypervisor:
          - `io.katacontainers.config.hypervisor.path`
          - `io.katacontainers.config.hypervisor.hypervisor_hash`
    
        - hypervisor control binary:
          - `io.katacontainers.config.hypervisor.ctlpath`
          - `io.katacontainers.config.hypervisor.hypervisorctl_hash`
    
        - jailer:
          - `io.katacontainers.config.hypervisor.jailer_path`
          - `io.katacontainers.config.hypervisor.jailer_hash`
    
    This change fixes the bug where specifying a custom hypervisor path via an
    asset annotation was having no effect.
    
    Fixes: #3030.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- virtcontainers/pkg/oci/utils.go
+++ virtcontainers/pkg/oci/utils.go
@@ -330,7 +330,11 @@ func SandboxID(spec specs.Spec) (string, error) {
 }
 
 func addAnnotations(ocispec specs.Spec, config *vc.SandboxConfig) error {
-	addAssetAnnotations(ocispec, config)
+	err := addAssetAnnotations(ocispec, config)
+	if err != nil {
+		return err
+	}
+
 	if err := addHypervisorConfigOverrides(ocispec, config); err != nil {
 		return err
 	}
@@ -345,17 +349,10 @@ func addAnnotations(ocispec specs.Spec, config *vc.SandboxConfig) error {
 	return nil
 }
 
-func addAssetAnnotations(ocispec specs.Spec, config *vc.SandboxConfig) {
-	assetAnnotations := []string{
-		vcAnnotations.KernelPath,
-		vcAnnotations.ImagePath,
-		vcAnnotations.InitrdPath,
-		vcAnnotations.FirmwarePath,
-		vcAnnotations.KernelHash,
-		vcAnnotations.ImageHash,
-		vcAnnotations.InitrdHash,
-		vcAnnotations.FirmwareHash,
-		vcAnnotations.AssetHashType,
+func addAssetAnnotations(ocispec specs.Spec, config *vc.SandboxConfig) error {
+	assetAnnotations, err := types.AssetAnnotations()
+	if err != nil {
+		return err
 	}
 
 	for _, a := range assetAnnotations {
@@ -366,6 +363,8 @@ func addAssetAnnotations(ocispec specs.Spec, config *vc.SandboxConfig) {
 
 		config.Annotations[a] = value
 	}
+
+	return nil
 }
 
 func addHypervisorConfigOverrides(ocispec specs.Spec, config *vc.SandboxConfig) error {
